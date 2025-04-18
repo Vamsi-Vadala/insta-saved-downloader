9f0fb1b14e97fe5c0dfcd1f944cb5b1c0f77de2b
Vamsi Vadala <40667761+Vamsi-Vadala@users.noreply.github.com>
2025-04-18 07:11:59 -0700

    Added multiple changes to existing to correct the functionality of 'download' method

--- src/InstaDownloader.py
+++ src/InstaDownloader.py
@@ -11,13 +11,19 @@ import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from tqdm import tqdm
+from loginHandler import InstaLoginHandler as Login
+import pickle
 
 class InstagramDownloader:
     def __init__(self, max_workers=10):
         self.max_workers = max_workers
         self.no_video_url_count = 0  # Counter for failed video extractions
         self.lock = threading.Lock()  # Thread-safe lock for counter updates
-        self.pbar = None  # Progress bar
+        self.pbar = None  # Progress bar\
+        login = Login()
+        self.insta_username = login.get_username()
+        self.insta_cookies = pickle.load(open("../instagram_cookies.pkl", "rb"))
+        self.chrome_service = login.chrome_service
     
     def metadata(self, video_url):
         try:
@@ -51,30 +57,56 @@ class InstagramDownloader:
     def download(self, url, video_only=False, audio_only=False):
         try:
             chrome_options = Options()
-            chrome_options.add_argument("--headless=new")
+            # chrome_options.add_argument("--headless=new")
             chrome_options.set_capability("goog:loggingPrefs", {"performance": "ALL"})
             
-            driver = webdriver.Chrome(options=chrome_options)
+            driver = webdriver.Chrome(options=chrome_options,service=self.chrome_service)
+            driver.get('https://www.instagram.com')
+            for cookie in self.insta_cookies:
+                driver.add_cookie(cookie)
+            driver.refresh()
             driver.get(url.strip())
             time.sleep(5)  # Wait for page load
             
             logs = driver.get_log("performance")
             driver.quit()
             
-            video_urls = [json.loads(log["message"]) for log in logs 
-                          if "Network.requestWillBeSent" in json.loads(log["message"])["message"]["method"]
-                          and ".mp4" in json.loads(log["message"])["message"]["params"]["request"]["url"]]
+            # print(f'fetched logs successfully starting to Inspect the logd to extract urls......')
+
+            # TODO Delete the following lines
+            # Delete this ------------------------
             
+            # with open('logs.json','w') as f:
+            #     # print(f'Writing logs to logs.json file......')
+            #     json.dump(log_file, f, indent=4)
+            #     # print(f'Writing logs to logs.json file : Successful')
+            #     f.close()
+            # Delete this -------------------------
+            video_urls = []
+            log_file = [json.loads(log["message"]) for log in logs 
+                        if "Network.requestWillBeSent" in json.loads(log["message"])["message"]["method"]]
+            
+            for log_message in log_file:
+                try:
+                    url = log_message['message']['params']['request']['url']
+                    if '.mp4' in url:
+                        video_urls.append(url)
+                except:
+                    continue
+
+            print(f' url extraction successfull. starting to download the videos from extracted urls......')
+
             if not video_urls:
                 with self.lock:
                     self.no_video_url_count += 1
                 print(f"No video URL found for {url}. Count: {self.no_video_url_count}")
                 return
             
-            cleaned_urls = set(re.sub(r"&bytestart=\\d+&byteend=\\d+", "", u) for u in video_urls)
+            cleaned_urls = set(re.sub(r"&bytestart=.*&byteend=.*", "", u) for u in video_urls)
             max_video_bitrate, max_audio_bitrate = 0, 0
             final_video_url, final_audio_url = None, None
 
+            print(f'celaned urls are :{cleaned_urls}')
             for u in cleaned_urls:
                 bitrate = self.metadata(u)
                 if bitrate['video'] > max_video_bitrate:
@@ -82,11 +114,17 @@ class InstagramDownloader:
                 if bitrate['audio'] > max_audio_bitrate:
                     max_audio_bitrate, final_audio_url = bitrate['audio'], u
             
+            print(f'extracted urls with highest resolution which is {max_video_bitrate} and {max_audio_bitrate}......')
+            
+            print(f'Extracting Title......')
             file_name = self.title(url)
+            print(f'Extracting Title successful {file_name}. Starting to download..........')
             if final_video_url and not audio_only:
                 urllib.request.urlretrieve(final_video_url, file_name + '.mp4', self.show_progress)
             if final_audio_url and not video_only:
                 urllib.request.urlretrieve(final_audio_url, file_name + '.mp3', self.show_progress)
+
+            self.merge(file_name)
         except Exception as e:
             print(f"Error processing {url}: {e}")
 
