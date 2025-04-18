ba37c5fd55abec42b8807457fe3ecedf07341dc4
Vamsi Vadala <40667761+Vamsi-Vadala@users.noreply.github.com>
2025-04-18 07:11:05 -0700

    Corrected error in abs filepath, now it correctly detects drivers, cookies.

--- src/loginHandler.py
+++ src/loginHandler.py
@@ -33,28 +33,26 @@ class InstaLoginHandler:
         driver.quit()  # Close the browser
 
     def get_username(self):
-        if not os.path.exists("instagram_cookies.pkl"):
-            print("No cookies found. Please log in first.")
+        if not os.path.exists("../instagram_cookies.pkl"):
+            print(f"No cookies found. Please log in first.")
             self.login_handler()
         print(f'Using cookies from {os.path.abspath("instagram_cookies.pkl")}')
-        chrome_options = Options()
-        driver = webdriver.Chrome(service=self.chrome_service, options=chrome_options)
-        cookies = pickle.load(open("instagram_cookies.pkl", "rb"))
-        driver.get("https://www.instagram.com")
-        time.sleep(5)
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-        driver.refresh()
-        time.sleep(5)
-        driver.get("https://www.instagram.com/accounts/edit/")
-        time.sleep(5)
-        bs4_obj = bs4.BeautifulSoup(driver.page_source, "html.parser")
-        with open("username.html", "wb") as f:
-            f.write(bs4_obj.prettify("utf-8"))
-            f.close()
-        driver.quit()
+        # chrome_options = Options()
+        # driver = webdriver.Chrome(service=self.chrome_service, options=chrome_options)
+        # cookies = pickle.load(open("../instagram_cookies.pkl", "rb"))
+        # driver.get("https://www.instagram.com")
+        # time.sleep(5)
+        # for cookie in cookies:
+        #     driver.add_cookie(cookie)
+        # driver.refresh()
+        # time.sleep(5)
+        # driver.get("https://www.instagram.com/accounts/edit/")
+        # time.sleep(5)
+        # bs4_obj = bs4.BeautifulSoup(driver.page_source, "html.parser")
+        # with open("username.html", "wb") as f:
+        #     f.write(bs4_obj.prettify("utf-8"))
+        #     f.close()
+        # driver.quit()
 
         return USER_NAME
     
-if __name__ == "__main__":
-    InstaLoginHandler().get_username()
