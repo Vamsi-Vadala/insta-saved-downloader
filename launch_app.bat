f327054bf4b1e7ceb54230933b6cb5168649651c
Vamsi-Vadala <vadalavamsi.tv@gmail.com>
2025-05-25 12:46:13 -0700

    Added exec for Linux and Windows

---
+++ launch_app.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d %~dp0\src
+
+REM Activate venv from root folder
+IF EXIST "..\.venv\Scripts\activate.bat" (
+    call "..\.venv\Scripts\activate.bat"
+)
+
+REM Open in default browser
+start http://localhost:5000
+
+REM Run the Flask app using waitress
+python InstaWebDownloader.py
